66511116e895ea471a3e7cfffcf35ad9579ea51d
arietis-blue <arietis20324@gmail.com>
2023-06-27 18:11:56 +0900

    データベース操作

--- server/App_Folder/models.py
+++ server/App_Folder/models.py
@@ -4,17 +4,17 @@ from django.db import models
 # モデルクラスを作成
 class Papers(models.Model):
     
-    Paper_ID     = models.CharField(max_length=100)             #論文ID                   arxiv
-    Title_En     = models.TextField(blank=True, null=True)      #英語タイトル               arxiv
-    Title_Ja     = models.EmailField(max_length=100)            #日本語タイトル             deepl
-    Content_En   = models.TextField()                           #abstract                arxiv
-    Content_Ja   = models.TextField()                           #abstract                deepl
-    Search_num   = models.IntegerField(blank=True, null=True)   #検索回数   
+    Paper_ID     = models.CharField(max_length=1000)             #論文ID                   arxiv
+    Title_En     = models.TextField()      #英語タイトル               arxiv
+    Title_Ja     = models.TextField()            #日本語タイトル             deepl
+    Content_En   = models.TextField(null = True)                           #abstract                arxiv
+    Content_Ja   = models.TextField(null = True)                           #abstract                deepl
+    Search_num   = models.IntegerField(null=True)   #検索回数   
 
 
 class Keywords(models.Model):
 
-    Paper_ID     = models.CharField(max_length=100)             #論文ID   arxiv
+    Paper_ID     = models.CharField(max_length=1000)             #論文ID   arxiv
     Keyword      = models.TextField()                              
     Description  = models.TextField()   
 
@@ -28,6 +28,6 @@ class Profile(models.Model):
 
 class Reads(models.Model):
 
-    Paper_ID     = models.CharField(max_length=100)             #論文ID
+    Paper_ID     = models.CharField(max_length=1000)             #論文ID
     Reader_ID    = models.IntegerField(null=True)               #読んだ人のID               
     Date         = models.DateTimeField()                       #日時
