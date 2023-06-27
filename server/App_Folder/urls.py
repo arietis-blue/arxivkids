18ff0507aac6057cd9fa86c44322a2cc08ad9293
nabeshinabe <nabeoka.takuto.25@gmail.com>
2023-06-27 15:33:35 +0900

    Djangoのパスを繋げる

---
+++ server/App_Folder/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+# from .views import DiaryByIDView,  DiaryCommentView, DiaryView
+
+urlpatterns = [
+    # path('diaries/', DiaryView.as_view()),
+    # path('diaries/<int:diary_id>/', DiaryByIDView.as_view()),
+    # path('diaries/CommentView/<int:diary_id>', DiaryCommentView.as_view()),
+]
