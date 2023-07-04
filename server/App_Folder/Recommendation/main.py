47dbb21891a914103e3c753bed0ed1e765dd98a8
nabeshinabe <nabeoka.takuto.25@gmail.com>
2023-07-04 15:22:35 +0900

    Recomend Start

---
+++ server/App_Folder/Recommendation/main.py
@@ -0,0 +1,35 @@
+import pandas as pd
+# import arxiv_analytics as ax
+
+
+# arxiv_data = []
+
+
+# 各個人のジャンルをもとに推薦
+# ログインしていないなら、人気の論文表示
+
+# user_id = -1
+# if(ログインしていたら):
+#     user_id = 000000000
+#     arxiv_data = (user_idの読んだ論文リスト)
+
+
+# ログインしていない場合
+# if(user_id==-1):
+    # query = ax.ArxivQuery()
+    # query.max_results(10)
+    # query.sortby("submittedDate")
+    # query.sortorder("descending")
+    # query.category("cs.LG").AND().abstract("deep learning") # 各自に合わせた推薦ではない為不要
+
+# else:
+    # ジャンル抽出 カウント
+    # キーワード抽出
+
+    # query = ax.ArxivQuery()
+    # query.max_results(10)
+    # query.sortby("submittedDate")
+    # query.sortorder("descending")
+    # query.category("cs.LG").AND().abstract("deep learning")    
+
+# 
