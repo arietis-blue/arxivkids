472d1a498633f7cd938ea44b8f621e6018404507
kyoto-u-mori <mori.kazuhito.63r@st.kyoto-u.ac.jp>
2023-07-11 15:46:25 +0900

    修正: "main"を復活

--- server/App_Folder/api_call/search_paper.py
+++ server/App_Folder/api_call/search_paper.py
@@ -1,6 +1,17 @@
 from .arxiv_api.arxiv_search import get_arxiv_data
 from .deepl_api.deepl import translate_ja_list
 
+def main(query):
+    # arxivから論文の取得
+    paper_list = get_arxiv_data(query)
+    title_list = [paper["Title_En"] for paper in paper_list]
+    # タイトルをdeeplで翻訳
+    title_ja_list = translate_ja_list(title_list)
+    # "Title_Ja"として追加して返す
+    for i,paper in enumerate(paper_list):
+        paper["Title_Ja"] = title_ja_list[i]
+    return(paper_list)
+
 def get_papers(query):
     # arxivから論文の取得
     paper_list = get_arxiv_data(query)
