21d42611f05736e197ec5cb4d51826c9357e9393
kyoto-u-mori <mori.kazuhito.63r@st.kyoto-u.ac.jp>
2023-07-11 16:16:19 +0900

    修正: キーワード取得のプロンプトの修正 1. カテゴリの考慮 2. 論文内の情報だけでなく自分で情報も追加してく

--- server/App_Folder/api_call/gpt_api/gpt_getkeywords.py
+++ server/App_Folder/api_call/gpt_api/gpt_getkeywords.py
@@ -11,7 +11,7 @@ load_dotenv(Path(__file__).parent.parent.joinpath(".env"))
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # Textを受け取りキーワードと説明文のリストを返す
-def keywords(content):
+def keywords(content,category):
   time_sta = time.time()
   schema = {
   "type": "object",
@@ -27,7 +27,7 @@ def keywords(content):
             },
             "Description":{
               "type":"string",
-              "description":"200 Japanese Characters explaining the meaning of the KEYWORD"
+              "description":"200 Japanese Characters explaining the meaning of the Keyword"
             },
           }
                 }
@@ -35,11 +35,11 @@ def keywords(content):
   },
   "required": ["Keyword","Description"]
 }  
-  
+  category_prompt = ",\n".join(category)
   
   prompt = """
-           Task: Please extract five technical terms about following paper and its detailed description in Japanese. 
-           Limitation: each keyword description is in 200 Japanese Characters.
+           Task: I would like to acquire and explain technical terms so that beginners can understand the following paper. Please extract five technical terms in japanese. And create a description for that keyword in Japanese. However, please add an explanation of the word itself as well as how it is used in the abstract.
+           Limit: Each description must be written in around 200 Japanese characters.
            Format example:
            [
              {
@@ -67,7 +67,7 @@ def keywords(content):
   completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo-0613",
     messages=[
-      {"role": "system", "content": "You are an expert in the field of this paper."},
+      {"role": "system", "content": "You are an expert in the following academic areas\n" + category_prompt},
       {"role": "user", "content": prompt}
     ],
     functions=[{"name": "set_keywords_and_desctiption", "parameters": schema}],
@@ -85,10 +85,15 @@ def keywords(content):
   # 経過時間（秒）
   tim = time_end- time_sta
   print(tim)
-  print(result)
   return(result)
 
 # Example Usage
-# Text = '我々は、高レベルのコミュニケーションと低レベルのパスプランニングの両方に、事前に訓練された大規模言語モデル（LLM）の力を活用する、マルチロボット協調のための新しいアプローチを提案する。ロボットは、タスク戦略を議論し、集団的に推論するためにLLMを装備している。LLMはサブタスク計画とタスク空間ウェイポイント経路を生成し、マルチアームモーションプランナーにより軌道計画を加速する。また、衝突チェックのような環境からのフィードバックを提供し、LLMエージェントがコンテキスト内で計画とウェイポイントを改善するように促す。評価のために、我々はRoCoBenchを導入する。RoCoBenchは、エージェントの表現と推論のためのテキストのみのデータセットとともに、幅広いマルチロボット協調シナリオをカバーする6タスクベンチマークである。RoCoBenchの全てのタスクにおいて高い成功率を達成し、タスクセマンティクスの変化にも適応する。我々のダイアログ設定は高い解釈性と柔軟性を提供し、実世界の実験では、RoCoは簡単にヒューマンインザループを組み込むことができ、ユーザはロボットエージェントとコミュニケーションし、協力してタスクを完了することができる。ビデオとコードはプロジェクトのウェブサイトhttps://project-roco.github.io'
-# keywords(Text)
+Text = '我々は、高レベルのコミュニケーションと低レベルのパスプランニングの両方に、事前に訓練された大規模言語モデル（LLM）の力を活用する、マルチロボット協調のための新しいアプローチを提案する。ロボットは、タスク戦略を議論し、集団的に推論するためにLLMを装備している。LLMはサブタスク計画とタスク空間ウェイポイント経路を生成し、マルチアームモーションプランナーにより軌道計画を加速する。また、衝突チェックのような環境からのフィードバックを提供し、LLMエージェントがコンテキスト内で計画とウェイポイントを改善するように促す。評価のために、我々はRoCoBenchを導入する。RoCoBenchは、エージェントの表現と推論のためのテキストのみのデータセットとともに、幅広いマルチロボット協調シナリオをカバーする6タスクベンチマークである。RoCoBenchの全てのタスクにおいて高い成功率を達成し、タスクセマンティクスの変化にも適応する。我々のダイアログ設定は高い解釈性と柔軟性を提供し、実世界の実験では、RoCoは簡単にヒューマンインザループを組み込むことができ、ユーザはロボットエージェントとコミュニケーションし、協力してタスクを完了することができる。ビデオとコードはプロジェクトのウェブサイトhttps://project-roco.github.io'
+category = [
+        "Robotics",
+        "Artificial Intelligence",
+        "Machine Learning"
+    ]
+output = keywords(Text,category)
+print(output)
 
