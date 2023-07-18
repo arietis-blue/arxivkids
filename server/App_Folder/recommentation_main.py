235a6214530c024db2cf6544b1ba45b2f3351cb4
nabeshinabe <nabeoka.takuto.25@gmail.com>
2023-07-18 18:01:44 +0900

    Initialize Recommendation

--- server/App_Folder/recommentation_main.py
+++ server/App_Folder/recommentation_main.py
@@ -4,18 +4,23 @@
 from pdb import set_trace
 from .operate_database import search_rank
 
-def recommendation():
+
 # 各個人のジャンルをもとに推薦
-# ログインしていないなら、人気の論文表示
+def recommendation():
+    # user_idを-1で初期化
     user_id = -1
     # if(ログインしていたら):
     #     user_id = 000000000
     #     arxiv_data = (user_idの読んだ論文リスト)
     
-    # ログインしていない場合
+    # ログインしていない場合、人気の論文を表示
     if(user_id == -1):
         json_result = search_rank()
-        return json_result["rank"]
+
+        if json_result == {}: # 何も論文が検索されていないなら空列を返す
+            return json_result
+        else: # 論文が検索されていたらrankを返す
+            return json_result["rank"]
 
     # else:
         # ジャンル抽出 カウント
