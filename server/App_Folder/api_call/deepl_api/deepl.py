cf43abcfc155a412b5e5d1df9fdcc8ba36217e9d
kyoto-u-mori <mori.kazuhito.63r@st.kyoto-u.ac.jp>
2023-07-23 18:43:19 +0900

    追加: 日本語でも論文検索可能にする処理

--- server/App_Folder/api_call/deepl_api/deepl.py
+++ server/App_Folder/api_call/deepl_api/deepl.py
@@ -6,8 +6,6 @@ import os
 
 load_dotenv(Path(__file__).parent.parent.joinpath(".env"))
 API_KEY =os.environ["DEEPL_API_KEY"]# 自身の API キーを指定
-source_lang = 'EN'
-target_lang = 'JA'
 
 # 英語の文字列のリストを受け取り、翻訳した文章を追加したリストとして返す
 def translate_ja_list(content_list):
@@ -16,8 +14,8 @@ def translate_ja_list(content_list):
     params = {
                 'auth_key' : API_KEY,
                 'text' : content_list,
-                'source_lang' : source_lang, # 翻訳対象の言語
-                "target_lang": target_lang  # 翻訳後の言語
+                'source_lang' : 'EN', # 翻訳対象の言語
+                "target_lang": 'JA'  # 翻訳後の言語
             }
     # リクエストを投げる
     request = requests.post("https://api.deepl.com/v2/translate", data=params) # URIは有償版, 無償版で異なるため要注意
@@ -31,8 +29,23 @@ def translate_ja(text):
     params = {
                 'auth_key' : API_KEY,
                 'text' : text,
-                'source_lang' : source_lang, # 翻訳対象の言語
-                "target_lang": target_lang  # 翻訳後の言語
+                'source_lang' : 'EN', # 翻訳対象の言語
+                "target_lang": 'JA'  # 翻訳後の言語
+            }
+    # リクエストを投げる
+    request = requests.post("https://api.deepl.com/v2/translate", data=params) # URIは有償版, 無償版で異なるため要注意
+    result = request.json()
+    result = result["translations"][0]["text"]
+    return result
+
+# 日本語の文字列を受け取り、翻訳した英語の文章を返す
+def translate_en(text):
+    # パラメータの指定
+    params = {
+                'auth_key' : API_KEY,
+                'text' : text,
+                'source_lang' : 'JA', # 翻訳対象の言語
+                "target_lang": 'EN'  # 翻訳後の言語
             }
     # リクエストを投げる
     request = requests.post("https://api.deepl.com/v2/translate", data=params) # URIは有償版, 無償版で異なるため要注意
