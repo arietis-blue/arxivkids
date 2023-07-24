ac57a1dee8dfc24ef2fddc749f11f2e0ec34e952
nabeshinabe <nabeoka.takuto.25@gmail.com>
2023-07-24 20:19:25 +0900

    画面が真っ白になるバグ修正

--- client/src/stores/choosedPaperInfo.js
+++ client/src/stores/choosedPaperInfo.js
@@ -5,7 +5,7 @@ export const useChoosedPaperInfoStore = defineStore('choosed-paper-info', () => 
     const Title_En = ref("")
     const Title_Ja = ref("")
     const Published = ref("")
-    const Pdf_url = ref()
+    const Pdf_url = ref("")
     const Authors = ref([])
     const Categories = ref([])
 
--- client/src/stores/fivePapers.js
+++ client/src/stores/fivePapers.js
@@ -3,6 +3,6 @@ import { ref } from "vue"
 
 export const useFivePapersStore = defineStore('five-papers', () => {
     // const fivePapers = ref([])   //() or ([])都行貌似
-    const fivePapers = ref()
+    const fivePapers = ref("")
     return { fivePapers }
 })
